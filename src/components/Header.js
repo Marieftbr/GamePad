0754325d9e164bcbe64e75cc971ec2f1bcd1d1b0
Marie Gigaud <mariegoulouzelle@gmail.com>
2022-03-04 12:08:13 +0100

    add conditions for user connected or not and some links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,48 @@
 import React from "react";
 import GamepadLogo from "../img/gampad-logo.png";
+import { useNavigate } from "react-router-dom";
 
-export default function Header() {
+export default function Header(props) {
+  const navigate = useNavigate();
   return (
     <div className="header">
       <span className="logo-header-container">
-        <img className="header-logo" src={GamepadLogo} />
-        <h1 className="logo-text">Gamepad</h1>
+        <img
+          className="header-logo"
+          src={GamepadLogo}
+          alt="logo"
+          onClick={() => navigate("/")}
+        />
+        <h1 className="logo-text" onClick={() => navigate("/")}>
+          Gamepad
+        </h1>
       </span>
       <div className="btn-area">
-        <span className="collection-btn">My Collection</span>
-        <span className="login-btn">Login</span>
+        {props.token ? (
+          <span className="user-infos-container">
+            <span
+              className="collection-btn"
+              onClick={() => navigate("/myCollection")}
+            >
+              My Collection
+            </span>
+            <p>{props.userName}</p>
+            <img
+              className="header-profile-picture"
+              src={props.userPicture}
+              alt="user"
+            />
+          </span>
+        ) : (
+          <span>
+            <span className="collection-btn" onClick={() => navigate("/login")}>
+              My Collection
+            </span>
+            <span className="login-btn" onClick={() => navigate("/login")}>
+              Login
+            </span>
+          </span>
+        )}
       </div>
     </div>
   );
